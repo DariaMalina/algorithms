48ed4d7b472181dd509c284ae7120cea22cb9150
DariaMalina <Cmoke68blyat>
2021-06-04 13:07:31 +0300

    fix bag

--- Yandex_karter/Trigon/b_trigon.test.ts
+++ Yandex_karter/Trigon/b_trigon.test.ts
@@ -2,12 +2,12 @@ import {triangle} from "./b_trigon";
 
 test('triangle test', () => {
 
-    expect(triangle(3, 2, 1)).toStrictEqual("YES")
+    expect(triangle(3, 2, 2)).toStrictEqual("YES")
 })
 
 test('naturalness check',()=>{
     expect(triangle(8, 7, 0)).toStrictEqual("NO")
 })
 test('sum of parties',()=>{
-    expect(triangle(1, 1, 1)).toStrictEqual("NO")
+    expect(triangle(1, 1, 1)).toStrictEqual("YES")
 })
