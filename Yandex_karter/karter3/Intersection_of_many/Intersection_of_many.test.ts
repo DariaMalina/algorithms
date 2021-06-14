d9f43d5e45eb85df2023be8e67d8f5cb680d8bee
DariaMalina <Cmoke68blyat>
2021-06-14 21:20:59 +0300

    final view of the task

--- Yandex_karter/karter3/Intersection_of_many/Intersection_of_many.test.ts
+++ Yandex_karter/karter3/Intersection_of_many/Intersection_of_many.test.ts
@@ -8,6 +8,9 @@ describe('intersection of many', () => {
         expect(intersectionOfMany([1, 2, 6 ,4, 5, 7  ],[10, 2 ,3, 4, 8])).toBe('2,4')
     })
     test('intersectionOfMany3',()=>{
-        expect(intersectionOfMany([],[])).toBe('2')
+        expect(intersectionOfMany([],[])).toBe('')
+    })
+    test('intersectionOfMany4',()=>{
+        expect(intersectionOfMany([1, 2, 6 ,4, 5, 7  ],[10, 9 ,3, 11, 8])).toBe('')
     })
 })
