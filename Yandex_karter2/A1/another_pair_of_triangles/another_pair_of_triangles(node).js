7b6df2bf1444919848213e58123fdd65060895ed
Daria Malina <gluto68@gmail.com>
2021-10-16 11:30:38 +0300

    add: `(date)`

--- Yandex_karter2/A1/another_pair_of_triangles/another_pair_of_triangles(node).js
+++ Yandex_karter2/A1/another_pair_of_triangles/another_pair_of_triangles(node).js
@@ -1,3 +1,29 @@
-function f() {
-    
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+let count = 0;
+let arr = []
+const callback = (line) => {
+    count += 1;
+
+    if (count !==1 ) {
+        arr.push(Number(line))
+        console.log(triangles(arr))
+        process.exit()
+    }
+};
+rl.on('line', callback)
+const ANSWER = {
+    Inf: 'INF',
+    No: 'NO'
+}
+
+function triangles(arr) {
+    const x1=arr[0][1];
+    const x2=arr[2][3];
+    const x3=arr[4][5];
+    const x4=arr[6][7];
+    return 'l'
 }
